8cb46e50b90b39757195a25db46f33b106e5044b
Chinmay Shivratriwar <shivratriwarchinmay@gmail.com>
2025-06-09 18:27:21 +0530

    [CNS-CBS] - Addressing PR review comments

--- src/main/java/com/chinmayshivratriwar/cache_comparison/controller/ComparisonController.java
+++ src/main/java/com/chinmayshivratriwar/cache_comparison/controller/ComparisonController.java
@@ -4,7 +4,10 @@ import com.chinmayshivratriwar.cache_comparison.service.CacheBenchmarkService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
