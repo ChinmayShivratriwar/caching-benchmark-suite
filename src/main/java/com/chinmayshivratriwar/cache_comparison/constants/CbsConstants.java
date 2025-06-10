5e955c7a6019f766e3dca8a74953affc00dc8601
Chinmay Shivratriwar <shivratriwarchinmay@gmail.com>
2025-06-10 07:40:02 +0530

    [CNS-CBS] - Support for Blob / Large Objects

--- src/main/java/com/chinmayshivratriwar/cache_comparison/constants/CbsConstants.java
+++ src/main/java/com/chinmayshivratriwar/cache_comparison/constants/CbsConstants.java
@@ -7,4 +7,5 @@ public class CbsConstants {
     }
 
     public static final String CBS_ERROR_EXPRESSION = "[CNS - CBS Suite] An error occurred | errorCode: {} | errorDescription: {}";
+    public static final String CBS_WARN_EXCEPTION_EXPRESSION = "[CNS - CBS Suite] An exception is thrown in class - {}";
 }
